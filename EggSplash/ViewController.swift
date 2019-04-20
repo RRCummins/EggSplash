65e77612fdcd60ac656572dec3af9f6aea12d469
RRCummins <brokenrhino@gmail.com>
2019-04-20 15:26:37 -0400

    Initial Commit

---
+++ EggSplash/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  EggSplash
+//
+//  Created by Ryan Cummins on 4/20/19.
+//  Copyright © 2019 Them Rhinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    super.viewDidLayoutSubviews()
+    
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
